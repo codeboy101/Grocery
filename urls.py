c614d70c9fe701a64708186ac386c02f48199661
Tushar <codeboy101@users.noreply.github.com>
2016-04-10 17:11:21 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,9 +1,7 @@
 from django.conf.urls import url , include
 from . import views
 
-urlpatterns=[
-	url(r'^$',views.show_list,name='show_list'),
-	url(r'^addItem/$',views.addItem,name='addItem'),
-	url(r'^removeItem/(?P<pk>\d+)/$',views.removeItem,name='removeItem'),
-	url(r'^removeItemW/$',views.removeItemW,name='removeItem'),
+urlpatterns = [
+	url(r'^$',views.home,name='home'),
+	url(r'^greet/$',views.greet,name='greet'),
 ]
